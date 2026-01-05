a58d12ba2c22a2148d22c6cda6ecda11e197a2a0
Argeu Souza <argeusousaptbr@gmail.com>
2026-01-05 13:22:47 -0300

    feat: component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+import { ClienteService } from './services/cliente.service';
+import { Cliente } from './models/cliente';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  clienteService = inject(ClienteService);
+
+  // Lista de Clientes (Observable)
+  clientes$: Observable<Cliente[]> = this.clienteService.getClientes();
+
+  // Objeto para formulário
+  clienteAtual: Cliente = { 
+    nome: '',
+    cpf: '',
+    email: '',
+    telefone: '',
+    dataCriacao: ''
+   };
+   editando = false; // Controle de estado - Create or Update
+
+   salvar() {
+    if (this.editando) {
+      this.clienteService.updateCliente(this.clienteAtual)
+      .then(() => this.limparForm())
+      .catch(err => console.error(err));
+    }
+    else {
+      this.clienteService.addCliente(this.clienteAtual)
+      .then(() => this.limparForm())
+      .catch(err => console.error(err));
+    }
+   }
+
+   editar(cliente: Cliente) {
+    this.clienteAtual = { ...cliente };
+    this.editando = true;
+   }
+
+   deletar(id: string) {
+    this.clienteService.deleteCliente(id);
+   }
+
+   limparForm() {
+    this.clienteAtual = { 
+      nome: '',
+      cpf: '',
+      email: '',
+      telefone: '',
+      dataCriacao: ''
+    }
+   }
+
   title = 'rici-crud';
 }
