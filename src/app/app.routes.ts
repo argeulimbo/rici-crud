5d839a47786eed1631e9c7b4aa0081d30bd0eafc
Argeu Souza <argeusousaptbr@gmail.com>
2026-01-06 18:58:27 -0300

    refact: recreate component cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
+// Basic
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// Imports Components
+
+// Components
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import { NgModule } from '@angular/core';
+
 
 export const routes: Routes = [
     {
         path: '', component: NavComponent, canActivate: [],
         children: [
-            { path: 'home', component: HomeComponent }
+            { path: 'home', component: HomeComponent },
         ]
     }
-    // { 
-    //     path: 'home', component: HomeComponent
-    // }
 ]
 
 @NgModule({
